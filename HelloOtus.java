26242a48d999a670234cfd46b4627673ea9760fd
leklerk1985 <98700407+leklerk1985@users.noreply.github.com>
2023-01-11 13:36:21 +0300

    Add files via upload

---
+++ HelloOtus.java
@@ -0,0 +1,9 @@
+//import com.google.guava;
+import static com.google.common.math.IntMath.factorial;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        int fact = factorial(3);
+        System.out.println("factorial(3) = " + fact);
+    }
+}
