c58a7d69eed96df5d7dec8d44c22d540b31c0514
leklerk1985 <98700407+leklerk1985@users.noreply.github.com>
2023-03-19 15:52:57 +0300

    Delete TestLoggingInterface.java

--- TestLoggingInterface.java
+++
@@ -1,7 +0,0 @@
-package homework;
-
-public interface TestLoggingInterface {
-    public void calculation(int param1);
-    public void calculation(int param1, int param2);
-    public void calculation(int param1, int param2, int param3);
-}
