a0c6d39393da22ab076b2cf59a19244e21c8262f
leklerk1985 <98700407+leklerk1985@users.noreply.github.com>
2023-03-19 15:52:39 +0300

    Delete Runner.java

--- Runner.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class Runner {
-    public static void main(String[] args) {
-        TestLogging.createInstanceUsingProxy().calculation(100);
-        TestLogging.createInstanceUsingProxy().calculation(100, 200);
-        TestLogging.createInstanceUsingProxy().calculation(100, 200, 300);
-    }
-}
