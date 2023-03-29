2549edb26ea1eb28fcdc29dd3c00ced6beb2791a
leklerk1985 <98700407+leklerk1985@users.noreply.github.com>
2023-03-29 15:58:39 +0300

    Add files via upload

---
+++ L15-structuralPatterns/src/main/java/ru/otus/HomeWork.java
@@ -0,0 +1,63 @@
+package ru.otus;
+
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.*;
+import java.util.List;
+
+public class HomeWork {
+
+    /*
+     Реализовать to do:
+       1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
+       2. Сделать процессор, который поменяет местами значения field11 и field12
+       3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
+             Секунда должна определяьться во время выполнения.
+             Тест - важная часть задания
+             Обязательно посмотрите пример к паттерну Мементо!
+       4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
+          Уже есть заготовка - класс HistoryListener, надо сделать его реализацию
+          Для него уже есть тест, убедитесь, что тест проходит
+     */
+
+    public static void main(String[] args) {
+        /*
+           по аналогии с Demo.class
+           из элеменов "to do" создать new ComplexProcessor и обработать сообщение
+         */
+
+        var processors = List.of(new ProcessorExceptionAtEvenSecond(),
+                new ProcessorSwapField11AndField12(),
+                new ProcessorConcatFields());
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {System.out.println(ex.getMessage());});
+
+        var listenerPrinter = new ListenerPrinterConsole();
+        var listenerHistory = new HistoryListener();
+        complexProcessor.addListener(listenerPrinter);
+        complexProcessor.addListener(listenerHistory);
+
+        var message = new Message.Builder(1L)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .field13(new ObjectForMessage())
+                .build();
+
+        var result = complexProcessor.handle(message);
+        System.out.println("result:" + result);
+        System.out.println();
+
+        listenerHistory.printHistory();
+
+        complexProcessor.removeListener(listenerPrinter);
+        complexProcessor.removeListener(listenerHistory);
+    }
+}
